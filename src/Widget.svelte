9020e18aa8a582c0ada7122c511cf6d484bedbaf
anamst <ana.milic.strkalj@gmail.com>
2020-10-16 10:18:40 +0200

    modal

--- src/Widget.svelte
+++ src/Widget.svelte
@@ -1,31 +1,36 @@
 <script>
+  import Modal from "./components/Modal.svelte";
   import "./main.css";
 
-
+  let showModal = false;
+  
   window.onload = function toggleOverlay() {
   let btn = document.getElementById("btnOverlay");
   let overlay = document.getElementById("widget");
+  
     btn.addEventListener("click", function () {
       overlay.style.display = "block";
-    });
-    overlay.addEventListener("click", function () {
-      overlay.style.display = "none";
+      btn.style.display = "none";
+      showModal = true;
     });
   }
   
 </script>
 
 <section>
-  <div>
+  <div class="flex place-content-end m-5 p-4">
     <button
-      on:click = {toggleOverlay()}
+      on:click = {toggleOverlay}
       id="btnOverlay"
       class="inline-block bg-blue-500 py-2 px-6 m-auto text-gray-100 uppercase
         text-lg rounded">tip me</button>
   </div>
 
+  {#if showModal}
+    <Modal on:close="{() => showModal = false}" />
+  {/if}
   <div
     id="widget"
-    class="fixed inset-0 flex hidden overflow-hidden w-screen h-screen z-50 bg-black
+    class="fixed inset-0 flex hidden overflow-hidden w-screen h-screen z-0 bg-black
       bg-opacity-50 transition-opacity duration-200" />
 </section>
--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,4 +1,21 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  const close = () => dispatch('close');
 
 </script>
 
+<div id="modal" role="dialog" aria-modal="true" class="modal absolute w-40 h-40 right-auto left-auto rounded shadow-md bg-white bg-opacity-100 z-50 " >
+  <div class="modal-header flex justify-between items-baseline">
+    <slot class="modal-title"></slot>
+    <button on:click={close} id="close" class="modal-close">close</button>
+  </div>
+  <hr>
+  <div class="modal-body">
+    <slot></slot>
+  </div>
+  <div class="modal-footer flex">
+    <slot></slot>
+  </div>
+</div>
