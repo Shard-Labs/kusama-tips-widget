3d9d412ee41ef3fb276f71e79eff716bfa48830a
anamst <ana.milic.strkalj@gmail.com>
2020-10-21 11:10:12 +0200

    multistep

--- src/Widget.svelte
+++ src/Widget.svelte
@@ -2,35 +2,43 @@
   import Modal from "./components/Modal.svelte";
   import "./main.css";
 
-  let showModal = false;
+  let showModal = false; 
   
-  window.onload = function toggleOverlay() {
+  window.onload = function openOverlay() {
   let btn = document.getElementById("btnOverlay");
   let overlay = document.getElementById("widget");
-  
     btn.addEventListener("click", function () {
       overlay.style.display = "block";
       btn.style.display = "none";
       showModal = true;
     });
   }
+
+  function closeOverlay() {
+    let overlay = document.getElementById("widget");
+    showModal = false;
+    overlay.style.display = "none";
+  }
+ 
   
 </script>
 
 <section>
   <div class="flex place-content-end m-5 p-4">
     <button
-      on:click = {toggleOverlay}
+      on:click = {openOverlay}
       id="btnOverlay"
       class="inline-block bg-blue-500 py-2 px-6 m-auto text-gray-100 uppercase
-        text-lg rounded">tip me</button>
+        text-lg rounded">tip me
+    </button>
   </div>
 
   {#if showModal}
-    <Modal on:close="{() => showModal = false}" />
+    <Modal on:close={closeOverlay} />
   {/if}
+  
   <div
     id="widget"
-    class="fixed inset-0 flex hidden overflow-hidden w-screen h-screen z-0 bg-black
+    class="fixed inset-0 flex hidden overflow-hidden w-screen h-screen z-10 bg-black
       bg-opacity-50 transition-opacity duration-200" />
 </section>
--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,21 +1,28 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from "svelte";
+  import MultistepModal from "./MultistepModal.svelte";
 
   const dispatch = createEventDispatcher();
-  const close = () => dispatch('close');
-
+  const close = () => dispatch("close");
 </script>
 
-<div id="modal" role="dialog" aria-modal="true" class="modal absolute w-40 h-40 right-auto left-auto rounded shadow-md bg-white bg-opacity-100 z-50 " >
-  <div class="modal-header flex justify-between items-baseline">
-    <slot class="modal-title"></slot>
-    <button on:click={close} id="close" class="modal-close">close</button>
-  </div>
-  <hr>
-  <div class="modal-body">
-    <slot></slot>
-  </div>
-  <div class="modal-footer flex">
-    <slot></slot>
+<div class="modal-continer relative">
+  <div
+    id="modal"
+    role="dialog"
+    aria-modal="true"
+    class="modal absolute w-1/3 inset-x-0 mx-auto rounded shadow-md bg-white
+    bg-opacity-100 z-50 ">
+    <div class="modal-header px-8 py-6 flex justify-between items-baseline">
+      <slot class="modal-title text-xl text-gray-900 leading 8" />
+      <button on:click={close} id="close" class="modal-close ">close</button>
+    </div>
+    <hr />
+    <div class="modal-body px-8 py-6">
+      <MultistepModal />
+    </div>
+    <div class="modal-footer px-8 py-6 flex">
+      <slot />
+    </div>
   </div>
 </div>
